2b92cbb6de395317a78d93e30b8ad08c127649e8
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-05-28 21:07:39 -0500

    add Category entity

--- src/main/java/com/ironman/Main.java
+++ src/main/java/com/ironman/Main.java
@@ -1,7 +1,25 @@
 package com.ironman;
 
+import com.ironman.entity.Category;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Category category = new Category();
+
+        category.setId(1L);
+        category.setName("Category 1");
+        category.setDescription("Description 1");
+        category.setUrlKey("category-1");
+        category.setState("A");
+
+
+        System.out.println("Category Name: " + category.getName());
+
+        Category category2 = new Category(2L, "Category 2", "Description 2", "category-2", "A", null, null);
+        System.out.println("Category 2 Description: " + category2.getDescription());
+
     }
 }

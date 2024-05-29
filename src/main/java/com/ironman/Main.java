9b9f2ca45e120d34c801a837ef1130ee62bc44ed
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-05-28 21:38:29 -0500

    add ConnectionCore

--- src/main/java/com/ironman/Main.java
+++ src/main/java/com/ironman/Main.java
@@ -1,5 +1,6 @@
 package com.ironman;
 
+import com.ironman.dao.ConnectionCore;
 import com.ironman.entity.Category;
 
 
@@ -7,19 +8,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Category category = new Category();
+        ConnectionCore connection = new ConnectionCore();
 
-        category.setId(1L);
-        category.setName("Category 1");
-        category.setDescription("Description 1");
-        category.setUrlKey("category-1");
-        category.setState("A");
+        connection.getConnection();
 
-
-        System.out.println("Category Name: " + category.getName());
-
-        Category category2 = new Category(2L, "Category 2", "Description 2", "category-2", "A", null, null);
-        System.out.println("Category 2 Description: " + category2.getDescription());
-
+        System.out.println("Connection successful!");
     }
 }
